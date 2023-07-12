4688545933b815ed616f7e0b0e908c5e642851b4
AnthonyFuller <24512050+AnthonyFuller@users.noreply.github.com>
2023-07-12 20:00:09 +0100

    feat(hook): add console and game detection

--- hook/src/lib.rs
+++ hook/src/lib.rs
@@ -3,14 +3,38 @@ mod dinput8;
 use dinput8::setup_dinput8;
 
 use std::ffi::c_void;
-use windows::Win32::{Foundation::HMODULE, System::SystemServices::DLL_PROCESS_ATTACH};
+use windows::Win32::{
+    Foundation::{HMODULE, BOOL},
+    System::{
+        SystemServices::DLL_PROCESS_ATTACH,
+        LibraryLoader::GetModuleHandleA,
+        Diagnostics::Debug::GetTimestampForLoadedLibrary,
+        Console::{AllocConsole, AttachConsole},
+        Threading::GetCurrentProcessId
+    }
+};
 
 #[no_mangle]
-pub extern "system" fn DllMain(_inst_dll: HMODULE, reason: u32, _reserved: *const c_void) -> bool {
+pub extern "stdcall" fn DllMain(_inst_dll: HMODULE, reason: u32, _reserved: *const c_void) -> BOOL {
     if reason == DLL_PROCESS_ATTACH {
         setup_dinput8();
 
-        // TODO: Add game detection + hooks.
+        // TODO: Make showing a console configurable
+        unsafe {
+            if !(AttachConsole(GetCurrentProcessId()).as_bool() || AllocConsole().as_bool()) {
+                panic!("[COBRA//HOOK] Unable to create console!");
+            }
+        }
+
+        let timestamp = unsafe { GetTimestampForLoadedLibrary(GetModuleHandleA(None).unwrap()) };
+
+        match timestamp {
+            0x5047356A => println!("[COBRA//HOOK] HITMAN: Sniper Challenge (STEAM) found!"),
+            0x5149E0B4 => println!("[COBRA//HOOK] HITMAN: Absolution (STEAM) found!"),
+            _ => println!("[COBRA//HOOK] Unknown game version found.")
+        }
+
+        // TODO: Add hooks.
     }
-    true
+    true.into()
 }
